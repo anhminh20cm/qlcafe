1a6106f0f5c5d10bb45d1ca8786845593517a6c8
ANHMINH\chila <chilamo9999@gmail.com>
2022-11-20 14:40:16 +0700

    no message

---
+++ QLQuanCafe/3.PL/FrmAcountProfile.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace _3.PL
+{
+    partial class FrmAcountProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txb_userName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txb_displayName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txt_passWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txt_newPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txt_reEnterPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txb_userName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(507, 47);
+            this.panel2.TabIndex = 1;
+            // 
+            // txb_userName
+            // 
+            this.txb_userName.Location = new System.Drawing.Point(212, 10);
+            this.txb_userName.Name = "txb_userName";
+            this.txb_userName.ReadOnly = true;
+            this.txb_userName.Size = new System.Drawing.Size(281, 27);
+            this.txb_userName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÊN ĐĂNG NHẬP:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txb_displayName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 47);
+            this.panel1.TabIndex = 2;
+            // 
+            // txb_displayName
+            // 
+            this.txb_displayName.Location = new System.Drawing.Point(212, 10);
+            this.txb_displayName.Name = "txb_displayName";
+            this.txb_displayName.Size = new System.Drawing.Size(281, 27);
+            this.txb_displayName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(14, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TÊN HIỂN THỊ: ";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txt_passWord);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(12, 118);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(507, 47);
+            this.panel3.TabIndex = 2;
+            // 
+            // txt_passWord
+            // 
+            this.txt_passWord.Location = new System.Drawing.Point(212, 10);
+            this.txt_passWord.Name = "txt_passWord";
+            this.txt_passWord.Size = new System.Drawing.Size(281, 27);
+            this.txt_passWord.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(14, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "MẬT KHẨU:";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.txt_newPass);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(12, 171);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(507, 47);
+            this.panel4.TabIndex = 2;
+            // 
+            // txt_newPass
+            // 
+            this.txt_newPass.Location = new System.Drawing.Point(212, 10);
+            this.txt_newPass.Name = "txt_newPass";
+            this.txt_newPass.Size = new System.Drawing.Size(281, 27);
+            this.txt_newPass.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(14, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "MẬT KHẨU MỚI:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.txt_reEnterPass);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(12, 224);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(507, 47);
+            this.panel5.TabIndex = 2;
+            // 
+            // txt_reEnterPass
+            // 
+            this.txt_reEnterPass.Location = new System.Drawing.Point(212, 10);
+            this.txt_reEnterPass.Name = "txt_reEnterPass";
+            this.txt_reEnterPass.Size = new System.Drawing.Size(281, 27);
+            this.txt_reEnterPass.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(14, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "NHẬP LẠI MKM:";
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(12, 296);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(94, 50);
+            this.btn_update.TabIndex = 3;
+            this.btn_update.Text = "CẬP NHẬT";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(425, 296);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(94, 50);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "THOÁT";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // FrmAcountProfile
+            // 
+            this.AcceptButton = this.btn_update;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_exit;
+            this.ClientSize = new System.Drawing.Size(547, 358);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "FrmAcountProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "thông tin cá nhân";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txb_userName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txb_displayName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txt_passWord;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txt_newPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox txt_reEnterPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
---
+++ QLQuanCafe/3.PL/FrmTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL
+{
+    public partial class FrmTableManger : Form
+    {
+        public FrmTableManger()
+        {
+            InitializeComponent();
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAcountProfile frmAcountProfile = new FrmAcountProfile();
+            frmAcountProfile.ShowDialog();
+
+
+        }
+
+        private void adminToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAdmin frmAdmin = new FrmAdmin();
+            frmAdmin.ShowDialog();
+        }
+    }
+}
